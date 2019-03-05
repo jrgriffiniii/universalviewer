7dead410978c7ac8431ddb1da1a8335440a37bf3
GriffinJ <jrgriffiniii@gmail.com>
2019-03-05 15:53:38 -0500

    Ensuring that the URI for the current canvas is used for the share button

--- src/modules/uv-shared-module/BaseExtension.ts
+++ src/modules/uv-shared-module/BaseExtension.ts
@@ -755,7 +755,8 @@ class BaseExtension implements IExtension {
     }
 
     getIIIFShareUrl(): string {
-        return this.helper.iiifResourceUri + "?manifest=" + this.helper.iiifResourceUri;
+        const canvas: Manifesto.ICanvas = this.helper.getCurrentCanvas();
+        return canvas.getCanonicalImageUri() + "?manifest=" + canvas.getCanonicalImageUri();
     }
 
     addTimestamp(uri: string): string{
